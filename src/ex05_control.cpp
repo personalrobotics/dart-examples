44c570cb3d8221accf6f3ad49b2ca5ae224f6449
Michael Koval <mkoval@cs.cmu.edu>
2016-07-06 12:07:28 -0500

    Started using RosJointStateClient.

--- src/ex05_control.cpp
+++ src/ex05_control.cpp
@@ -138,7 +138,7 @@ int main(int argc, char** argv)
   static const std::vector<std::string> jointNames{
     "rightShoulderPitch", "rightShoulderRoll", "rightShoulderYaw",
     "rightElbowPitch"};
-  static const Eigen::Vector4d trajectoryEndpoint{
+  static const Eigen::Vector4d goalPosition{
     0.161101, 0.214281, 0.2127471, 1.419200};
 #endif
   static const std::chrono::milliseconds controlPeriod{20};
@@ -146,25 +146,26 @@ int main(int argc, char** argv)
   static const double goalTimeTolerance{0.5};
   static const double startupTimeTolerance{1.0};
   static const double trajectoryDuration{5.};
+  static const size_t jointStateQueueLength{100};
 
-  ROS_INFO("Starting ROS node.");
+  ROS_INFO_STREAM("Starting ROS node.");
   ros::init(argc, argv, "ex05_control");
   ros::NodeHandle nh;
   
   if (!ros::Time::waitForValid(ros::WallDuration(startupTimeTolerance)))
     throw std::runtime_error("Timed out while waiting for valid ros::Time.");
 
-  ROS_INFO("Starting execution thread.");
+  ROS_INFO_STREAM("Starting execution thread.");
   ExecutorMultiplexer executorCallback;
   ExecutorThread executorThread{std::ref(executorCallback), controlPeriod};
 
-  ROS_INFO("Loading URDF from: %s", urdfUri.c_str());
+  ROS_INFO_STREAM("Loading URDF from: " << urdfUri.c_str());
   dart::utils::DartLoader urdfLoader;
   const auto resourceRetriever
     = std::make_shared<aikido::util::CatkinResourceRetriever>();
   const auto skeleton = urdfLoader.parseSkeleton(urdfUri, resourceRetriever);
 
-  ROS_INFO("Creating MetaSkeleton.");
+  ROS_INFO_STREAM("Creating MetaSkeleton.");
   std::vector<Joint*> metaSkeletonJoints;
   for (const auto& jointName : jointNames)
     metaSkeletonJoints.emplace_back(skeleton->getJoint(jointName));
@@ -172,52 +173,58 @@ int main(int argc, char** argv)
   const auto metaSkeleton = Group::create("RightShoulder");
   metaSkeleton->addJoints(metaSkeletonJoints, true, true);
 
-  ROS_INFO("Creating viewer.");
+  ROS_INFO_STREAM("Creating viewer.");
   InteractiveMarkerViewer viewer{markerTopic};
   viewer.addSkeleton(skeleton);
   viewer.setAutoUpdate(true);
+  // TODO: Create another Skeleton to visualize the desired position.
 
-  ROS_INFO("Creating JointState client.");
-  RosJointStateClient jointStateClient{skeleton, nh, jointStateTopic};
+  ROS_INFO_STREAM("Creating JointState client.");
+  RosJointStateClient jointStateClient{skeleton, nh, jointStateTopic,
+    jointStateQueueLength};
   executorCallback.addCallback(
     std::bind(&RosJointStateClient::spin, &jointStateClient));
 
-  ROS_INFO("Creating TrajectoryExecutor client.");
+  usleep(1e6); // TODO: Replace getLatestPosition with a blocking call.
+  const auto startPosition = jointStateClient.getLatestPosition(*metaSkeleton);
+  metaSkeleton->setPositions(startPosition);
+
+  ROS_INFO_STREAM("Creating TrajectoryExecutor client.");
   RosTrajectoryExecutor trajectoryClient{metaSkeleton, nh,
     trajectoryTopicNamespace, timestep, goalTimeTolerance};
   executorCallback.addCallback(
     std::bind(&RosTrajectoryExecutor::spin, &trajectoryClient));
 
-  ROS_INFO("Creating SplineTrajectory.");
+  ROS_INFO_STREAM("Creating SplineTrajectory.");
   const auto stateSpace = std::make_shared<MetaSkeletonStateSpace>(metaSkeleton);
   const auto trajectory = std::make_shared<SplineTrajectory>(stateSpace);
 
   Eigen::MatrixXd coefficients{metaSkeleton->getNumDofs(), 2};
-  coefficients.col(0) = trajectoryEndpoint;
-  coefficients.col(1).setZero();
+  coefficients.col(0) = startPosition;
+  coefficients.col(1) = (goalPosition - startPosition) / trajectoryDuration;
 
   auto state = stateSpace->createState();
   trajectory->addSegment(coefficients, trajectoryDuration, state);
 
-  ROS_INFO("Executing trajectory.");
+  ROS_INFO_STREAM("Executing trajectory.");
   auto trajectoryFuture = trajectoryClient.execute(trajectory);
 
-  ROS_INFO("Waiting for trajectory to finish.");
+  ROS_INFO_STREAM("Waiting for trajectory to finish.");
   trajectoryFuture.wait();
 
   try
   {
     trajectoryFuture.get();
-    ROS_INFO("Execution succeeded.");
+    ROS_INFO_STREAM("Execution succeeded.");
   }
   catch (const std::runtime_error& e)
   {
     ROS_ERROR("%s", e.what());
   }
 
-  ROS_INFO("Done. Press <Ctrl> + C to exit.");
+  ROS_INFO_STREAM("Done. Press <Ctrl> + C to exit.");
   ros::spin();
-  ROS_INFO("Exiting.");
+  ROS_INFO_STREAM("Exiting.");
 
   return 0;
 }
