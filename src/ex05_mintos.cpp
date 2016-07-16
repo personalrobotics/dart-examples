7181c826046a6ccfb5dcc71ee18592b92c98bd2a
Michael Koval <mkoval@cs.cmu.edu>
2016-07-15 20:14:11 -0400

    Added constraint to MinTOS.

--- src/ex05_mintos.cpp
+++ src/ex05_mintos.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <dart/dart.hpp>
 #include <aikido/rviz/InteractiveMarkerViewer.hpp>
+#include <aikido/constraint/TSR.hpp>
 #include <aikido/statespace/dart/MetaSkeletonStateSpace.hpp>
 #include <aikido/planner/mintos/MinTOS.hpp>
 
@@ -23,14 +24,27 @@ int main(int argc, char **argv) {
   startConfiguration << 3.68, -1.90, 0.00, 2.20, 0.00, 0.00, 0.00; // home
   robot.setConfiguration(rightArmSpace, startConfiguration);
 
+  const Eigen::Vector3d direction(0, 0, -1);
+  const auto distance = 0.3;
+  const auto endEffector = robot.getRightEndEffector();
   auto untimedTrajectory = robot.planToEndEffectorOffset(
-      rightArmSpace, robot.getRightEndEffector().get(),
-      Eigen::Vector3d(0, 0, -1),
-      0.3, planningTimeout);
+      rightArmSpace, endEffector.get(),
+      direction, distance, planningTimeout);
   if (!untimedTrajectory)
     throw std::runtime_error("Failed to find a solution");
 
-  aikido::constraint::Differentiable* constraint = nullptr;
+  // Debug
+  double epsilon = 0.01;
+  auto constraint = std::make_shared<aikido::constraint::TSR>();
+  constraint->mT0_w = dart::math::computeTransform(direction / direction.norm(),
+                                                   endEffector->getTransform().translation(),
+                                                   dart::math::AxisType::AXIS_Z);
+  constraint->mTw_e = constraint->mT0_w.inverse() * endEffector->getTransform(); 
+  constraint->mBw << -epsilon, epsilon, 
+      -epsilon, epsilon, std::min(0., distance), std::max(0., distance),
+      -epsilon, epsilon, -epsilon, epsilon, -epsilon, epsilon;
+  // Debug
+
   const auto velocityLimits = robot.getVelocityLimits(*rightArm);
   const auto accelerationLimits = robot.getAccelerationLimits(*rightArm);
   const auto constraintTolerance = 0.01;
